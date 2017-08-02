ee7ec05614417865eaf27759a5aa6e3fb913b485
EC2 Default User <ec2-user@ip-10-0-1-207.ap-northeast-1.compute.internal>
2017-08-02 16:26:34 +0000

    modify make_init.sh

--- make_init.sh
+++ make_init.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-ls common/init.el
-if [ $? -ne 0 ] then
+ls common/init.el > /dev/null
+if [ $? -ne 0 ] 
+then
     echo "error: common/init.el is not exist."
 fi
-cat common/init.el > ~/.emacs/init.el
-find . -name init.el | xargs cat >> ~/.emacs/init.el
+cat common/init.el > ~/.emacs.d/init.el
+find . -name init.el | xargs cat >> ~/.emacs.d/init.el
 
 
 echo "fin."
